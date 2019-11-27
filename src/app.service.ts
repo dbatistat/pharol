a162a5045ec11b93d549515f9b9cbf7c243ec9de
David Batista <dbatista.t@gmail.com>
2019-11-27 00:42:23 -0500

    feature(people): added validation for create and update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { People } from './interfaces/people';
 import { SearchPeopleDto } from './dto/search-people.dto';
 import { PeopleDto } from './dto/people.dto';
+import * as Joi from '@hapi/joi';
 
 @Injectable()
 export class AppService {
@@ -27,11 +28,21 @@ export class AppService {
   }
 
   async create(data: PeopleDto) {
+    const { error } = this.validateCreate().validate(data);
+    if (error) {
+      throw new BadRequestException(error.details);
+    }
+
     const newPeople = new this.peopleModel(data);
     return newPeople.save();
   }
 
   async update(id: string, data: PeopleDto) {
+    const { error } = this.validateUpdate().validate(data);
+    if (error) {
+      throw new BadRequestException(error.details);
+    }
+
     const updatePeople = await this.findProduct(id);
     updatePeople.rut = data.rut;
     updatePeople.rutChilen = data.rutChilen ? data.rutChilen : updatePeople.rutChilen;
@@ -78,5 +89,67 @@ export class AppService {
     }
 
     return await this.peopleModel.find(queryCond).exec();
+  }
+
+  private validateCreate() {
+    return Joi.object({
+      rut: Joi.string()
+        .alphanum()
+        .min(8)
+        .max(9)
+        .required(),
+
+      rutChilen: Joi.number()
+        .integer(),
+
+      name: Joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+
+      lastname: Joi.string()
+        .min(3)
+        .max(50)
+        .required(),
+
+      email: Joi.string()
+        .email()
+        .required(),
+
+      gender: Joi.string()
+        .valid('M')
+        .valid('F')
+        .required(),
+
+      addresses: Joi.array()
+        .items(Joi.object({ description: Joi.string().required() })),
+    });
+  }
+
+  private validateUpdate() {
+    return Joi.object({
+      rut: Joi.string()
+        .alphanum()
+        .min(8)
+        .max(9),
+
+      rutChilen: Joi.number()
+        .integer(),
+
+      name: Joi.string()
+        .min(3)
+        .max(30),
+
+      lastname: Joi.string()
+        .min(3)
+        .max(50),
+
+      email: Joi.string()
+        .email(),
+
+      gender: Joi.string()
+        .valid('M')
+        .valid('F'),
+    });
   }
 }
